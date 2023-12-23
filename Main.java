64bf379d82d73df1105811e2019df97b94edf96c
Harald Kleinschrot Colomer <kcharald@gmail.com>
2023-12-23 14:24:53 +0100

    Creation of some folders and main file

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String [] args) {
+        try {
+            System.out.println("Program started.");
+        } catch(Exception e) {
+            System.out.println("An error occurred.");
+        } finally {
+            System.out.println("Program finished.");
+        } // end of try, catch, finally
+    } // end of main
+} // end of Main
